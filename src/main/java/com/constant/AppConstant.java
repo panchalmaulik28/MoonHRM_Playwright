692aef63bf24d9574126344c320649174ade2408
panchalmauik28 <panchalmaulik28@gmail.com>
2025-02-04 09:32:37 +0530

    Jenkins
    Jenkins

--- src/main/java/com/constant/AppConstant.java
+++ src/main/java/com/constant/AppConstant.java
@@ -3,5 +3,5 @@ package com.constant;
 public class AppConstant {
 
 	public static final String LOGIN_SUCCESSFUL = "You have been logged in successfully";
-	public static final String LOGIN_FAILED = "Invalid Credentials";
+	public static final String LOGIN_FAILED = "Invalid Credentials!";
 }
